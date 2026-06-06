108f35314ffbd2df4fc95b7e3dc9cdf753b77e92
amandhulkar <amandhulkar0079@gmail.com>
2026-06-06 14:55:06 +0530

    Update MongoDB connection

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/chatingApp"); 
-
+    // await mongoose.connect("mongodb://localhost:27017/chatingApp"); 
+    await mongoose.connect(process.env.MONGO_URI);
+    
     console.log("MongoDB Connected");
   } catch (error) {
     console.log(error);
