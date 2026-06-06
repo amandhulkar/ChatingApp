80a4dedb25a27d6a1b3cab596eebfd8edc42958a
amandhulkar <amandhulkar0079@gmail.com>
2026-06-06 15:39:01 +0530

    debug mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
+     console.log("MONGO_URI:", process.env.MONGO_URI);
     // await mongoose.connect("mongodb://localhost:27017/chatingApp"); 
     await mongoose.connect(process.env.MONGO_URI);
     
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    // console.log(error);
+    console.log("MongoDB Error:", error);
   }
 };
 
