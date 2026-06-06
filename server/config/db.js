cbd3dcd130ba01094f775b2954aefd1819f0440c
amandhulkar <amandhulkar0079@gmail.com>
2026-06-06 16:10:14 +0530

    debug mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -4,8 +4,9 @@ const dbConnect = async () => {
   try {
      console.log("MONGO_URI:", process.env.MONGO_URI);
     // await mongoose.connect("mongodb://localhost:27017/chatingApp"); 
-    await mongoose.connect(process.env.MONGO_URI);
-    
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000,
+    });
     console.log("MongoDB Connected");
   } catch (error) {
     // console.log(error);
